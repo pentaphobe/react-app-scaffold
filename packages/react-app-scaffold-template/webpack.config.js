30ac996e0266cbe5902fbe4256548d2bba89d8f1
keilin.olsen <keilin@vectorflip.com>
2017-08-28 09:07:09 +1000

    initial attempt to encapsulate deps and webpack in template package
    - preference is to have Webpack, React (et al) in a common template base so that generated projects have the ability to receive build environment upgrades
    - having some pathing issues with `npm explore [...]` always force-resolving logical paths into physical ones (this may be a node bug)
    - works fine in generated projects (seemingly), but aiming for parity between that and the parent dev env

--- packages/react-app-scaffold-template/webpack.config.js
+++ packages/react-app-scaffold-template/webpack.config.js
@@ -5,14 +5,17 @@
 const path = require('path');
 const webpack = require('webpack');
 
+const parentRoot = path.join(process.env.PWD, '../../');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-	template: './src/client/index.html',
+	template: path.join(parentRoot, 'src/client/index.html'),
 	filename: 'index.html',
 	inject: 'body'
 });
 
 module.exports = {
+	context: parentRoot,
 	entry: [
 		'react-hot-loader/patch',
 
@@ -20,11 +23,11 @@ module.exports = {
 
 		'webpack/hot/only-dev-server',
 
-		'./src/client/index.jsx',
+		path.join(parentRoot, 'src/client/index.jsx'),
 	],
 
 	output: {
-		path: path.resolve('dist'),
+		path: path.resolve(path.join(parentRoot, '../dist')),
 		filename: 'index_bundle.js',
 		publicPath: '/'
 	},
@@ -34,7 +37,7 @@ module.exports = {
 	devServer: {
 		hot: true,
 
-		contentBase: path.resolve(__dirname, 'dist'),
+		contentBase: path.resolve('dist'),
 
 		publicPath: '/'
 	},
@@ -52,8 +55,9 @@ module.exports = {
 	resolve: {
 		extensions: ['.js', '.jsx'],
 		modules: [
-			path.resolve('./src'),
-			path.resolve('./node_modules')
+			path.resolve(path.join(parentRoot,'src')),
+			path.resolve(path.join(parentRoot,'node_modules')),
+			path.resolve(path.join(parentRoot,'node_modules/react-app-scaffold-template/node_modules'))
 		]
 	},
 	plugins: [
